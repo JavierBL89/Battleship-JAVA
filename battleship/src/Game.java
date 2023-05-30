1f631624217fa1daefbae1b6ed94a51bac49b71e
Javier <notri80@gmail.com>
2023-05-30 22:38:59 +0200

    Initial commit

---
+++ battleship/src/Game.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class Game {
+    public static void main(String[] args) throws Exception {
+        //GameFrame game = new JFrame();
+        System.out.println("Hello, World!");
+    }
+}
