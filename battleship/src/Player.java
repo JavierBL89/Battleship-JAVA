452b817092cc67090b83af4a8c6fe9e320427176
Javier <notri80@gmail.com>
2023-05-30 22:54:16 +0200

    Set initial attributes and methods to Player clas

---
+++ battleship/src/Player.java
@@ -0,0 +1,14 @@
+public class Player {
+    // vars
+    String name;
+
+    //contructor
+    Player (){
+     name = "";
+    }
+
+    // set
+    public void setName(String name){
+        this.name = name;
+    }
+}
