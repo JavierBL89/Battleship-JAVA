a77b215e1b1870dba64527bd75f0418f061e1e3d
Javier <notri80@gmail.com>
2023-05-30 23:00:23 +0200

    Set initial attributes and contructor to GameFrame class

--- battleship/src/Game.java
+++ battleship/src/Game.java
@@ -2,7 +2,6 @@ import javax.swing.JFrame;
 
 public class Game {
     public static void main(String[] args) throws Exception {
-        //GameFrame game = new JFrame();
-        System.out.println("Hello, World!");
+        GameFrame game = new GameFrame();
     }
 }
--- battleship/src/GameFrame.java
+++ battleship/src/GameFrame.java
@@ -1,5 +1,17 @@
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
+    //vars 
+    int width = 800;
+    int height = 600;
+    
+    //constructor
+    GameFrame(){
+        this.setSize(width, height);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
     
 }
