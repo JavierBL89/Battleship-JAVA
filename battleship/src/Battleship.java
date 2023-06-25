3872d107b83f79e0efe7a0c234352dfddbce03b3
Javier <notri80@gmail.com>
2023-06-25 13:08:06 +0200

    Check occupied cells. If new ship cells are used, generate new random ships till

--- battleship/src/Battleship.java
+++ battleship/src/Battleship.java
@@ -10,9 +10,6 @@ public class Battleship {
     String[] startCordenates = new String[2];
     String[] direction = {"down", "right"};
 
-    List<String> ocupateTiles = new ArrayList();
-
-
 
     String x;
     String y;
@@ -46,6 +43,8 @@ public class Battleship {
 
             // set playerName value
             this.playerName = playerName;
+            List<String> ocupateTiles = new ArrayList<>();
+
             // loops through ships array(3 ships length), and generate a random ship per each index,...
             //.... and push it into the array
             for(int i=0; i < ships.length ;i++){
@@ -53,19 +52,19 @@ public class Battleship {
                 int next = i+1;
                 int[] nextTile = new int[2];
                 String[] tempShip = new String[2];
-
                 // generate a rondom number for x & y 
                 Random ran = new Random();
                 int n = ran.nextInt(2);
                 randomDirection = direction[n];
                 x = Integer.toString(ran.nextInt(10));
                 y = Integer.toString(ran.nextInt(10));
-                // add x & y to create the radomShip and push the ship into the ships array
+                // add x & y to create the radomShip...
+                //.... and push the start ships coordenate into the ships array
                 startCordenates[0] = x;
                 startCordenates[1] = y;
-                
                 tempShip[0] = Arrays.toString(startCordenates);
-
+                // Create the next cell ship coordenatate..
+                // ..and push it into the tempShip array to continue ship 
                 if(randomDirection == "down"){
                     nextTile[0] = Integer.parseInt(x) + 1;
                     nextTile[1] = Integer.parseInt(y) ;
@@ -73,34 +72,24 @@ public class Battleship {
                     nextTile[0] = Integer.parseInt(x);
                     nextTile[1] = Integer.parseInt(y)+1;
                 }
+                tempShip[1] = Arrays.toString(nextTile);
 
-                if((Integer.parseInt(startCordenates[0]) + (shipLength - 1)) > 9){
-                      shipGenerator( playerName, ships);
-                }else{
-                    tempShip[1] = Arrays.toString(nextTile);
-                     if(i == 0 ){
-                     ships[0] = tempShip;
-                      // System.out.println(Arrays.toString(ships[0])  +" i");
-                }
-                     else if(i == 1 ){
-                        ships[1] = tempShip;
-                        // System.out.println(Arrays.toString(ships[0])  +" o");
-                        // System.out.println(Arrays.toString(ships[1])  +" o");
-                    }
-                    else if(i == 2 ){
-                     ships[2] = tempShip;
-                    //System.out.println(Arrays.toString(ships[2])  +" p");
+                // check if ship tiles are available
+                checkCellAvailability(playerName, ships, tempShip, ocupateTiles);
 
-                     }
-
-                }
-        //         System.out.println(i + " ITERATION");
-        //         System.out.println(Arrays.toString(tempShip) + "TEMPSHIP");
-        //         for(int j=0; j < ships.length;j++){
-        //                 System.out.println(Arrays.toString(ships[j]) + j + " w");
-        //   }
+                // If new cells ship are available,.. 
+                //..check if they are within the board limits, else generate new ships
+                 if((Integer.parseInt(startCordenates[0]) + (shipLength - 1) > 9 || 
+                                     (Integer.parseInt(startCordenates[1]) + (shipLength - 1) > 9))){
+                    shipGenerator( playerName, ships);
+                 }else{
+                   ships[i] = tempShip;
+                  ocupateTiles.add(tempShip[0]);
+                  ocupateTiles.add(tempShip[1]);
+                 }
 
             }
+
             if(playerName == "Compu"){
                     compuShips = ships;
             }else if(playerName == "Player"){
@@ -109,6 +98,33 @@ public class Battleship {
 
         }
 
+         /***
+         * Check used cells, if any of the new ship cells are in used,
+         *  call shipGenerator and generate new ships
+         *   Otherwise add the new ship cells into ocupateTiles array,...
+         *  ...and keep the shipgenerator going the next ship
+         */
+        public void checkCellAvailability(String playerName, String[][] ships, String[] tempShip,List<String> ocupateTiles){
 
+            if(ocupateTiles.size() == 0){
+                    ocupateTiles.add(tempShip[0]);
+                    ocupateTiles.add(tempShip[1]);
+                }else if(ocupateTiles.size() > 0){
+                    for(int h=0;h<ocupateTiles.size();h++){
+                       if(ocupateTiles.get(h).contains(tempShip[0])){
+                           System.out.println("First ship title matches an occupied title");
+                           System.out.println("Generating a new entire ship..");
+                           shipGenerator(playerName, ships);
+                       }else if(ocupateTiles.get(h).contains(tempShip[1])){ 
+                            System.out.println("First ship title matches an occupied title");
+                            System.out.println("Generating a new entire ship..");
+                           shipGenerator(playerName, ships);
+                    }else{
+                            continue;
+                       }
+                    }
+                }
+
+        }
 }
 
