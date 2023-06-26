5f76928700a72140c5bf0df228bba1e1aed3090a
Javier <notri80@gmail.com>
2023-06-26 22:04:23 +0200

    Get user guess input and convert it into an array of coordenates

--- battleship/src/Game.java
+++ battleship/src/Game.java
@@ -1,9 +1,19 @@
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class Game {
 
+
+    public static void takeTurn(String userInput){
+        String[] userGuess = new String[2];
+       // userGuess = textField.getText().trim();
+        userGuess[0] = userInput.substring(0, 1);
+        userGuess[1] = userInput.substring(2, 3);
+        System.out.print(userGuess[0] + " " +userGuess[1]);
+
+    }
     
     public static void main(String[] args) throws Exception {
         Battleship createShips = new Battleship();
--- battleship/src/GameFrame.java
+++ battleship/src/GameFrame.java
@@ -1,19 +1,28 @@
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
-public class GameFrame extends JFrame{
+public class GameFrame extends JFrame implements ActionListener{
     //vars 
     int width = 800;
     int height = 600;
     Board compuBoard;
     Board playerBoard;
+    JTextField textField;
     //constructor
     GameFrame(){
+
+
         // set window
         this.setSize(width, height);
         this.setResizable(false);
@@ -55,10 +64,41 @@ public class GameFrame extends JFrame{
        // ADD BOARDS INTO CONTAINERS
        //playerContainer.add(playerBoard);
        //compuContainer.add(compuBoard);
-        
+
+       // Text field
+       this.textField = new JTextField(8);
+       textField.setBounds(250, 400, 280, 30); // to get height, set large font
+       textField.setFont(textField.getFont().deriveFont(20f));
+       textField.addKeyListener(new MyKeyAdapter());
+; // "this" is implementing ActionListener 
+       this.add(textField);
+
        this.add(playerBoard);
        this.add(compuBoard);
+        addKeyListener(new MyKeyAdapter());
+
        this.setVisible(true);
     }
-    
+
+    public class MyKeyAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            String userGuess;
+            if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                userGuess = textField.getText();
+               // System.out.print(userGuess);
+                Game.takeTurn(userGuess);
+            }
+
+        }
+
+   }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
 }
